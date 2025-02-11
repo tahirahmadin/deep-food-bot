155f61e46d4e4ead7034c02e4bad647083ecf7aa
Tahir Ahmad <tahirahmad.in@gmail.com>
2025-02-11 14:07:43 +0530

    height changes

--- src/components/DunkinOrderApp.tsx
+++ src/components/DunkinOrderApp.tsx
@@ -490,8 +490,8 @@ export const DunkinOrderApp: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen relative flex items-center justify-center bg-gray-50">
-      <div className="relative bg-[#FFF5F2] w-full h-screen max-w-md overflow-hidden flex flex-col">
+    <div className="min-h-[100vh] relative flex items-center justify-center bg-gray-50">
+      <div className="relative bg-[#FFF5F2] w-full h-[100vh] max-w-md overflow-hidden flex flex-col">
         <Header
           onOpenPanel={() => setIsPanelOpen(true)}
           onCartClick={() => setIsCartOpen(!isCartOpen)}
