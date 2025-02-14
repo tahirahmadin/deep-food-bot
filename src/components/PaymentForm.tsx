d38426ecff8f1878a0f45e7c048b817be85f1519
Tahir Ahmad <tahirahmad.in@gmail.com>
2025-02-14 12:16:58 +0530

    chnages

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -29,7 +29,7 @@ const stripePromise = loadStripe(
   import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY ||
     "pk_test_51QnDfMRsmaUdhKRSXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
   {
-    stripeAccount: "acct_1QnDfMRsmaUdhKRS",
+    stripeAccount: "acct_1Qs3zeJDUPLwCpmp",
   }
 );
 
